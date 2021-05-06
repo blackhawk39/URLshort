d5f97c69756c0404ad24977568184a04e10871c2
sourabh thakur <sourabh7146@gmail.com>
2021-05-06 19:20:44 +0530

    first

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const mongoose=require('mongoose')
+const ShortUrl =require('./models/url')
+const murl = 'mongodb+srv://srbh:mangoatlas@cluster0.yotf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(murl,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( () => {
+        console.error(`Error connecting to the database.`);
+})
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+app.get('/', async(req,res) =>{
+    const shorturls= await ShortUrl.find()
+    res.render('index', { shorturls:shorturls})
+})
+app.post('/shortUrls', async (req,res)=> {
+    await ShortUrl.create({full: req.body.fullurl })
+    res.redirect('/')
+})
+app.listen(process.env.PORT || 4000)
